531a7011105fadb5ce2376afe436a1b6260ac60d
Laure Grisez <grisezlaure@gmail.com>
2023-04-16 21:08:27 +0200

    Adding the palisade part : test instance + io.jl

---
+++ palisade/src/generation.jl
@@ -0,0 +1,31 @@
+# This file contains methods to generate a data set of instances (i.e., sudoku grids)
+include("io.jl")
+
+"""
+Generate an n*n grid with a given density
+
+Argument
+- n: size of the grid
+- density: percentage in [0, 1] of initial values in the grid
+"""
+function generateInstance(n::Int64, density::Float64)
+
+    # TODO
+    println("In file generation.jl, in method generateInstance(), TODO: generate an instance")
+    
+end 
+
+"""
+Generate all the instances
+
+Remark: a grid is generated only if the corresponding output file does not already exist
+"""
+function generateDataSet()
+
+    # TODO
+    println("In file generation.jl, in method generateDataSet(), TODO: generate an instance")
+    
+end
+
+
+
---
+++ palisade/src/resolution.jl
@@ -0,0 +1,155 @@
+# This file contains methods to solve an instance (heuristically or with CPLEX)
+using CPLEX
+
+include("generation.jl")
+
+TOL = 0.00001
+
+"""
+Solve an instance with CPLEX
+"""
+function cplexSolve()
+
+    # Create the model
+    m = Model(with_optimizer(CPLEX.Optimizer))
+
+    # TODO
+    println("In file resolution.jl, in method cplexSolve(), TODO: fix input and output, define the model")
+
+    # Start a chronometer
+    start = time()
+
+    # Solve the model
+    optimize!(m)
+
+    # Return:
+    # 1 - true if an optimum is found
+    # 2 - the resolution time
+    return JuMP.primal_status(m) == JuMP.MathOptInterface.FEASIBLE_POINT, time() - start
+    
+end
+
+"""
+Heuristically solve an instance
+"""
+function heuristicSolve()
+
+    # TODO
+    println("In file resolution.jl, in method heuristicSolve(), TODO: fix input and output, define the model")
+    
+end 
+
+"""
+Solve all the instances contained in "../data" through CPLEX and heuristics
+
+The results are written in "../res/cplex" and "../res/heuristic"
+
+Remark: If an instance has previously been solved (either by cplex or the heuristic) it will not be solved again
+"""
+function solveDataSet()
+
+    dataFolder = "../data/"
+    resFolder = "../res/"
+
+    # Array which contains the name of the resolution methods
+    resolutionMethod = ["cplex"]
+    #resolutionMethod = ["cplex", "heuristique"]
+
+    # Array which contains the result folder of each resolution method
+    resolutionFolder = resFolder .* resolutionMethod
+
+    # Create each result folder if it does not exist
+    for folder in resolutionFolder
+        if !isdir(folder)
+            mkdir(folder)
+        end
+    end
+            
+    global isOptimal = false
+    global solveTime = -1
+
+    # For each instance
+    # (for each file in folder dataFolder which ends by ".txt")
+    for file in filter(x->occursin(".txt", x), readdir(dataFolder))  
+        
+        println("-- Resolution of ", file)
+        readInputFile(dataFolder * file)
+
+        # TODO
+        println("In file resolution.jl, in method solveDataSet(), TODO: read value returned by readInputFile()")
+        
+        # For each resolution method
+        for methodId in 1:size(resolutionMethod, 1)
+            
+            outputFile = resolutionFolder[methodId] * "/" * file
+
+            # If the instance has not already been solved by this method
+            if !isfile(outputFile)
+                
+                fout = open(outputFile, "w")  
+
+                resolutionTime = -1
+                isOptimal = false
+                
+                # If the method is cplex
+                if resolutionMethod[methodId] == "cplex"
+                    
+                    # TODO 
+                    println("In file resolution.jl, in method solveDataSet(), TODO: fix cplexSolve() arguments and returned values")
+                    
+                    # Solve it and get the results
+                    isOptimal, resolutionTime = cplexSolve()
+                    
+                    # If a solution is found, write it
+                    if isOptimal
+                        # TODO
+                        println("In file resolution.jl, in method solveDataSet(), TODO: write cplex solution in fout") 
+                    end
+
+                # If the method is one of the heuristics
+                else
+                    
+                    isSolved = false
+
+                    # Start a chronometer 
+                    startingTime = time()
+                    
+                    # While the grid is not solved and less than 100 seconds are elapsed
+                    while !isOptimal && resolutionTime < 100
+                        
+                        # TODO 
+                        println("In file resolution.jl, in method solveDataSet(), TODO: fix heuristicSolve() arguments and returned values")
+                        
+                        # Solve it and get the results
+                        isOptimal, resolutionTime = heuristicSolve()
+
+                        # Stop the chronometer
+                        resolutionTime = time() - startingTime
+                        
+                    end
+
+                    # Write the solution (if any)
+                    if isOptimal
+
+                        # TODO
+                        println("In file resolution.jl, in method solveDataSet(), TODO: write the heuristic solution in fout")
+                        
+                    end 
+                end
+
+                println(fout, "solveTime = ", resolutionTime) 
+                println(fout, "isOptimal = ", isOptimal)
+                
+                # TODO
+                println("In file resolution.jl, in method solveDataSet(), TODO: write the solution in fout") 
+                close(fout)
+            end
+
+
+            # Display the results obtained with the method on the current instance
+            include(outputFile)
+            println(resolutionMethod[methodId], " optimal: ", isOptimal)
+            println(resolutionMethod[methodId], " time: " * string(round(solveTime, sigdigits=2)) * "s\n")
+        end         
+    end 
+end
